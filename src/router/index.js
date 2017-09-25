1dd486fa448c1fa0567729eca3b129057ca0d937
messi <zzli@jinhetech.com>
2017-09-25 10:46:45 +0800

    --save

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Index from '@/components/Index'
+import Table from '@/components/Table'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/index',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/table',
+      name: 'Table',
+      component: Table
     }
   ]
 })
