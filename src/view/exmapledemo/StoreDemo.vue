6f3cfcc8cc129341edec591e3ca0f61bb628ba60
jcxu@jinhetech.com <jcxu@jinhetech.com>
2017-09-25 20:29:43 +0800

    --save

---
+++ src/view/exmapledemo/StoreDemo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-input v-model="changeinput"></el-input>
+  </div>
+</template>
+
+<style lang="css">
+  
+</style>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'storedemo',
+    data () {
+      return {
+
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'storedemo'
+      ]),
+      changeinput () {
+        return this.storedemo.input
+      }
+    }
+  }
+</script>
