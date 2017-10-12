d43af773e7105a762228235dd818c8076387f7e6
zxy <xyzan@jinhetech.com>
2017-10-12 20:33:31 +0800

    save

--- src/store/modules/sidebar.js
+++ src/store/modules/sidebar.js
@@ -1,11 +1,17 @@
+import Cookies from 'js-cookie'
 const sidebar = {
   state: {
     sidebar: {
-      opened: true
+      opened: !+Cookies.get('sidebarStatus')
     }
   },
   mutations: {
     TOGGLE_SIDEBAR: state => {
+      if (state.sidebar.opened) {
+        Cookies.set('sidebarStatus', 1)
+      } else {
+        Cookies.set('sidebarStatus', 0)
+      }
       state.sidebar.opened = !state.sidebar.opened
     }
   },
